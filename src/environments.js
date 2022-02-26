edf991b6c4cba477189630faf9fb4da420b0d11e
emgrow <egrow2@nd.edu>
2022-02-25 20:25:33 -0500

    api keys

---
+++ src/environments.js
@@ -0,0 +1,6 @@
+module.exports = {
+    APPLICATION_ID: "5pnKQ73IUwzCOzHKGlnI1LfnQ8Yu72mw0W3JmwCq",
+    JAVASCRIPT_KEY: "Wh3x3jsVXUTdIHAKNkONlS6pSkyi8mn1cUpNM2TT",
+    SERVER_URL: "https://parseapi.back4app.com/"
+  };
+  
